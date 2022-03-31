a4e28f61f4b06ace7e6d08ed6534910f03911173
deepak9597 <93966765+deepak9597@users.noreply.github.com>
2022-03-31 18:15:33 +0530

    some change

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -19,7 +19,7 @@ function HomeScreen(props) {
             <Text style={styles.text}>{props.counters.value}</Text>
             <Button title='incrment value' onPress={() => props.incrment()} />
             <Button title='decrment value' onPress={() => props.decrment()} />
-            <Text style={styles.text}>You have {props.items.current.length} items with value</Text>
+            <Text style={styles.text}>You have {props.items.current.length} items with value and some change</Text>
             {props.items.current.map((item, index) => (
                 <View>
                     <Text key={item} style={styles.text}>{item}</Text>
